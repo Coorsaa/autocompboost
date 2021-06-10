f39e1814dcb47c5a6d28a51b23efd7a72102322e
Stefan Coors <stefan.coors@gmx.net>
2021-06-11 00:11:18 +0200

    fixed param_set

--- R/param_set.R
+++ R/param_set.R
@@ -10,14 +10,14 @@ autocompboost_default_params = function(task_type) {
     return(
       ps(
         classif.rpart.cp = p_dbl(lower = 0, upper = 1),
-        classif.rpart.maxdepth = p_int(lower = 1, upper = 30, tag = "budget")
+        classif.rpart.maxdepth = p_int(lower = 1, upper = 30, tags = "budget")
       )
     )
   } else if (task_type == "regr") {
     return(
       ps(
         regr.rpart.cp = p_dbl(lower = 0, upper = 1),
-        regr.rpart.maxdepth = p_int(lower = 1, upper = 30, tag = "budget")
+        regr.rpart.maxdepth = p_int(lower = 1, upper = 30, tags = "budget")
       )
     )
   }
